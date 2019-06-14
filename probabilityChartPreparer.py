9c42beb1e8d6799f30cbe688bd8b9a1ca83f49a2
wlai0611 <32148858+wlai0611@users.noreply.github.com>
2019-06-13 22:53:09 -0400

    Add files via upload

---
+++ probabilityChartPreparer.py
@@ -0,0 +1,147 @@
+#Lines 18 to 84 was translated  
+#directly from Nick Brosowsky's commit to Entropy Typing
+# which was originally written in R. See AUTHORS page
+import pandas as pd
+#import os
+import numpy as np
+#os.chdir(r'C:\Users\Walt\Desktop\CognitionLab\letterFrequencyTextGenerator')
+file='ngrams1.csv'
+gram1=pd.read_csv(file,sep=',',index_col=0)
+gram1.head()
+#gimme the column as a series? how di i ignore the title?
+
+# well done!  Now how do we do this to alllc olumns
+def probability(x):
+    return x/x.sum()
+gram1prob=gram1.apply(probability)
+gram1prob.to_csv('gram1prob.csv')
+file='ngrams2.csv'
+gram2=pd.read_csv(file,sep=',',index_col=0)
+
+letters=['A','B','C','D','E','F','G','H','I','J','K',
+          'L','M','N','O',
+          'P','Q','R','S','T','U','V','W','X','Y','Z']
+ 
+ #all bigrams  MAKE THIS MORE EFFICIENT LATER
+bigrams=[]
+for letter in letters:
+     for letter2 in letters:
+         bigrams=bigrams+[letter+letter2]
+
+# lets save the row with NA and recreate gram2 without
+#we have an array of booleans when we run the above
+#before we save this, we need to save the row
+# now we must rename the index
+NArow=gram2[gram2.index.isnull()]
+NArow.index=['NA']
+#now we must recreate the gram2without the nan
+gram2= gram2[gram2.index.isnull()==False]
+#now append the NA row back in
+gram2=gram2.append(NArow)
+
+bigrams = np.array(bigrams)
+
+
+missingBigrams=np.array([])
+for bigram in bigrams:
+    if not(bigram in gram2.index):
+        missingBigrams=np.append(missingBigrams,bigram)
+        
+# make this automated based on how many missingvalues
+gramN=gram2
+missingNgrams=missingBigrams        
+# MAKE THIS MORE EFFICIENT LATER    
+missingDF = np.zeros((len(missingNgrams),gramN.shape[1]) )
+#replace 0 with 1
+missingDF[missingDF==0]=1
+#convert the array into DF
+missingDF=pd.DataFrame(missingDF,columns=list(gramN.columns))
+missingDF = missingDF.set_index(missingNgrams)
+gramN=gramN.append(missingDF)
+#get the first letter of each string in index list
+bigramIndex=gramN.index.tolist()
+# ok create the nMin1 and nLet columns and add to
+nMin1=np.array([])
+nLet=np.array([])
+for i in bigramIndex:
+    nMin1=np.append(nMin1,i[0])
+    nLet=np.append(nLet,i[1])
+#add these columns into the gramN column    
+gramN["nMin1"]=nMin1
+gramN["nLet"]=nLet
+# now we create a grouping object on nMin1
+prevLets=gramN.groupby("nMin1")
+# now we edit our probability function to take in each
+#we need to put all the n-1 together and do something
+gramNprob=prevLets.transform(probability)
+#add the nMin1 column to gramNprob
+#check probs, never compromise
+gramNprob=gramNprob.sort_index()
+gramN=gramN.sort_index()
+gramNprob['nMin1']=gramN["nMin1"]
+gramNprob.groupby(gramN['nMin1']).sum()
+gram2prob=gramNprob
+gram2prob.to_csv('gram2prob.csv')
+# they are all 1 for each n-1 and each letter position
+# this means that gramNprob has prob applied n-1 groups
+#load in the all possible trigrams
+allNgrams = np.load('gram3All.npy')
+#now find the missing make a df of 1s add to the norvig chart
+file='ngrams3.csv'
+gramN=pd.read_csv(file,sep=',',index_col=0)
+#check for NAN
+gramN[gramN.index.isnull()==True]
+#emptyDF means no nulls,
+#which ever allNgrams not in norvig's data, add to missingNgrams
+missingNgrams=np.array([])
+for nGram in allNgrams:
+    if not(nGram in gramN.index):
+        missingNgrams=np.append(missingNgrams,nGram)
+#doublecheck that we got all the ngrams in the missing thats not in norvig
+len(missingNgrams)+len(gramN.index)
+
+missingDF = np.zeros((len(missingNgrams),gramN.shape[1]) )
+#replace 0 with 1
+missingDF[missingDF==0]=1
+#convert the array into DF
+missingDF=pd.DataFrame(missingDF,columns=list(gramN.columns))
+missingDF = missingDF.set_index(missingNgrams)
+gramN=gramN.append(missingDF)
+# when we calculate probability for n-1, and we have n-1 all 0s...
+gramN[gramN==0]=1
+#ok next we 
+# ok create the nMin1 and nLet columns and add to
+#but in trigram and above, we need nmin1 to be multiple letters
+gramIndex=gramN.index.tolist()
+'abc'[-1]
+nMin1=np.array([])
+nLet=np.array([])
+for i in gramIndex:
+    nMin1=np.append(nMin1,i[:-1])
+    nLet=np.append(nLet,i[-1])
+
+#add these columns into the gramN column    
+gramN["nMin1"]=nMin1
+gramN["nLet"]=nLet
+# now we create a grouping object on nMin1
+prevLets=gramN.groupby("nMin1")
+# now we edit our probability function to take in each
+#we need to put all the n-1 together and do something
+gramNprob=prevLets.transform(probability)
+#check probs, never compromise
+# add nmin1 columnn to trigramprob
+gramNprob=gramNprob.sort_index()
+gramN=gramN.sort_index()
+gramNprob['nMin1']=gramN['nMin1']
+gramNprobSum = gramNprob.groupby(gramN['nMin1']).sum()
+gram3prob=gramNprob
+gram3prob.to_csv('gram3prob.csv')
+#what does nan mean? for the AA's ABCDEF prob function for posn, error
+#how to locate cells in the gramNprobsum that have this problem
+#and manuallly calculate probability
+#ok we got the probabilities, now lets do it for 4 quickly
+# lets identify which parts can be just run which hardcoded
+
+
+
+
---
+++ textGenerator.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 11 10:13:08 2019
+
+@author: Walt
+"""
+def frequencyTextGenerator(numberWords=10,gramLevel=3):
+    """ Returns array of English-like text.  First argument is number of words returned.  10 is default.  Second argument determines how much previous letters influence what will be the next letter.  You can choose 1,2 or 3.  The higher it is the more English like, the lower the more random unstructured.  1 or unigram means text is structured where the next letter is typed independently of previous letters.  2 or bigrams means only the immediately previous letter influences next letter.  3 or Trigrams means that the previous 2 letters influence next letter.  3 is default.  """
+    
+    import pandas as pd
+    
+    import numpy as np
+    if gramLevel>3:
+        raise ValueError('The Second Argument must be 1,2 or 3.  3 represents trigram level English and is the highest author has calculated so far.')
+    #os.chdir(r'C:\Users\Walt\Desktop\CognitionLab\letterFrequencyTextGenerator')
+    # break up letters into they are a lsit, HOW TO ADD STUFF OT LIST?
+    letters=['A','B','C','D','E','F','G','H','I','J','K',
+              'L','M','N','O',
+              'P','Q','R','S','T','U','V','W','X','Y','Z']
+    #when sved without specifying anything gram2probN
+    file='gram1prob.csv'
+    gram1prob=pd.read_csv(file,sep=',',index_col=0)
+    file='gram2prob.csv'
+    gram2prob=pd.read_csv(file,sep=',',index_col=0)
+    file='gram3prob.csv'
+    #look we have 
+    gram3prob=pd.read_csv(file,sep=',',index_col=0)
+    gram3prob.loc[gram3prob['nMin1'].isnull(),'nMin1']='NA'
+    #gram3prob.to_csv('gram3prob.csv')
+    #NAindexes=gram3prob[gram3prob['nMin1'].isnull()].index
+    #gram3prob.loc[NAindexes,'nMin1']
+    #create column list
+    #keep increasing the posn until posn = gram level then increase the start and posn til posn=len
+    #length/start:posn
+    #so lets say length 3 gram 2
+    
+    
+    
+        #so over here we must increase both posn and start which needs to be a variable
+        #we will keep incrementing posn so it iwll end  
+    lengthColumn={}
+    columns=np.array([])
+    length=9
+    
+    for length in range(1,10):
+        columns=np.array([])
+        start=1
+        for posn in range(1,(length+1)):    
+            if (posn<=gramLevel):
+                column= str(length) +'/'+str(start)+':'+str(posn)
+                    
+            else:
+                start=start+1
+                column= str(length) +'/'+str(start)+':'+str(posn)
+            columns=np.append(columns,column)
+        lengthColumn[length]=columns
+    #create the columns from gram2prob, gram3prob mapped to value of gramNprob
+    
+    
+    #lets be rational here we will need to inefficiently create this thing
+    # so lets make a for loop 
+    # lets bring in all the gramprobs charts first
+    
+    #ok we got all of em now lets loop through each column for gram1prob
+    #and make a dictionary
+    #empty dictionary
+    columnToTable=dict()
+    #get me the range of the gram1prob columns
+    
+    #ok we got the strings we want now time to create keys all to gram1prob
+    for key in gram1prob.columns.values[10:-18]:    
+        columnToTable[key]=gram1prob
+    for key in gram2prob.columns.values[9:-18]:    
+        columnToTable[key]=gram2prob
+    for key in gram3prob.columns.values[8:-17]:    
+        columnToTable[key]=gram3prob
+    #repeat for gram3prob and after we shoul
+    # now get me the columns manually from length
+        
+    
+    # how to filter a dataframe? got it.  automate the nMin1. ok now we need to loop
+    # this thing and set a special condition for the first
+    # in our loop what is held constant? length colns Changes? coln,table,previousLetters
+    # lets start with a premade array of strings first
+    # create dataframe of the proper dimensions
+    #old loop here
+    #lengths=np.random.choice(range(1,10),numberWords)
+    ###########
+            #this determines the number of rows
+    allWords=[]
+    #########Start here
+    for length in range(1,10):
+        numberWordsOfLengthN=numberWords
+        
+        # need to load the dictionary first
+        colns=lengthColumn[length]
+        # may be able to replace column names with actual'columnnames'variable
+        words=pd.DataFrame(index=range(0,numberWordsOfLengthN),columns=range(0,length))
+        words[words.isnull()]=''
+        # add the LENGTHS right before loop
+        # now lets construct the words from , we 
+        for coln in range(0,len(colns)):
+        	#get the column for the 3 letters
+        	#get up to the table=columnToTable and to the prefilter part then
+            #need the table dictinoary first
+            columnName=colns[coln]
+            table=columnToTable[columnName]
+            if coln==0:
+                prefilter=table.loc[:,[columnName]]
+                prob=prefilter[columnName].values.tolist()
+                words[0]=np.random.choice(letters,numberWordsOfLengthN,p=prob)
+                continue
+            else:
+                prefilter=table.loc[:,[columnName,'nMin1']]
+            # if coln is 0, populate the array with 3 values sampled using prefilter
+        # test inner loop with constant prefilter and columnName 
+            #may be able to replace len(words.index) with numberWordsOfLengthN
+            for word in range(0,len(words.index)):
+                    #we need the word stored as prevLetter
+                    #only gimme the row things thar arent nulls
+                prevLetList = words.iloc[[word]]
+                prevLetList=prevLetList.values.tolist()[0]
+                previousLetters=''.join(prevLetList)
+                previousLetters=previousLetters[-(gramLevel - 1):]
+                if gramLevel>1:
+                    prob=prefilter[prefilter['nMin1']==previousLetters][columnName]
+                else:
+                    prefilter=table.loc[:,[columnName]]
+                    prob=prefilter[columnName]
+                #ok next we need to add another column that is the second letter
+                #we need to assign the next column 
+                words.iloc[[word],[coln]]=np.random.choice(letters,p=prob.values.tolist())
+        #ok so now that we can manually do it, make it return multiple values
+        # SECRET IS DATAFRAME COLUMN [SPECIFIC COLUMN].v.tl()
+        # manually join the rows of the dataframe into a bunch of words
+        # first row
+        #just do it row by row for, when do we save the string? at the very end
+        
+        for i in range(0,len(words.index)):
+            rowWord=''.join(words.iloc[[i]].values.tolist()[0])
+            allWords.append(rowWord)
+    # ok so the issue is that the gramLevel 1 condition when coln not 0
+    # still goes to the inner loop which checks for nmin1 to give it a probability
+    # when gramLevel 1 all are done on the 26 73 chart without nmin1 level
+    # we need to use the column's probs without the filter
+    # put an if statement         
+            
+    # ALSO BIGRAM MAY HAVE AN ISSUE 
+    return(np.random.choice(allWords,numberWords))
+
+
+
+        
+        
+
